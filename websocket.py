9aec9d990e4d49cc639aaef6fe9255c432d65899
Journey <timtag1190@gmail.com>
2025-11-18 16:37:34 -0600

    Temporarily fix error code 1009 and 4010
    Fixes https://github.com/DevilXD/TwitchDropsMiner/issues/871 for now by doing the following:
    
    - Adding WEBSOCKET_TOPIC_BATCH_SIZE constant and use it to split LISTEN/UNLISTEN topic messages into smaller batches, sent sequentially.
    - Adding a short inter-batch delay (50ms) to be gentle with the server.
    
     This is just something quickly written for now until a better solution comes out. I'll more than likely revert this commit once the better solution comes out.

--- websocket.py
+++ websocket.py
@@ -12,7 +12,7 @@ import aiohttp
 
 from translate import _
 from exceptions import MinerException, WebsocketClosed
-from constants import PING_INTERVAL, PING_TIMEOUT, MAX_WEBSOCKETS, WS_TOPICS_LIMIT
+from constants import PING_INTERVAL, PING_TIMEOUT, MAX_WEBSOCKETS, WS_TOPICS_LIMIT, WEBSOCKET_TOPIC_BATCH_SIZE
 from utils import (
     CHARS_ASCII,
     task_wrapper,
@@ -178,8 +178,9 @@ class Websocket:
             except WebsocketClosed as exc:
                 if exc.received:
                     # server closed the connection, not us - reconnect
+                    # include close_message if present to help debug 1009 cases
                     ws_logger.warning(
-                        f"Websocket[{self._idx}] closed unexpectedly: {websocket.close_code}"
+                        f"Websocket[{self._idx}] closed unexpectedly: {websocket.close_code} ({websocket.close_message})"
                     )
                 elif self._closed.is_set():
                     # we closed it - exit
@@ -210,35 +211,44 @@ class Websocket:
         self.set_status(refresh_topics=True)
         auth_state = await self._twitch.get_auth()
         current: set[WebsocketTopic] = set(self.topics.values())
-        # handle removed topics
+        # handle removed topics (with batching)
         removed = self._submitted.difference(current)
         if removed:
             topics_list = list(map(str, removed))
             ws_logger.debug(f"Websocket[{self._idx}]: Removing topics: {', '.join(topics_list)}")
-            await self.send(
-                {
-                    "type": "UNLISTEN",
-                    "data": {
-                        "topics": topics_list,
-                        "auth_token": auth_state.access_token,
+            # send in batches
+            for i in range(0, len(topics_list), WEBSOCKET_TOPIC_BATCH_SIZE):
+                batch = topics_list[i : i + WEBSOCKET_TOPIC_BATCH_SIZE]
+                await self.send(
+                    {
+                        "type": "UNLISTEN",
+                        "data": {
+                            "topics": batch,
+                            "auth_token": auth_state.access_token,
+                        }
                     }
-                }
-            )
+                )
+                # be gentle on the server
+                await asyncio.sleep(0.05)
             self._submitted.difference_update(removed)
-        # handle added topics
+        # handle added topics (with batching)
         added = current.difference(self._submitted)
         if added:
             topics_list = list(map(str, added))
             ws_logger.debug(f"Websocket[{self._idx}]: Adding topics: {', '.join(topics_list)}")
-            await self.send(
-                {
-                    "type": "LISTEN",
-                    "data": {
-                        "topics": topics_list,
-                        "auth_token": auth_state.access_token,
+            for i in range(0, len(topics_list), WEBSOCKET_TOPIC_BATCH_SIZE):
+                batch = topics_list[i : i + WEBSOCKET_TOPIC_BATCH_SIZE]
+                await self.send(
+                    {
+                        "type": "LISTEN",
+                        "data": {
+                            "topics": batch,
+                            "auth_token": auth_state.access_token,
+                        }
                     }
-                }
-            )
+                )
+                # be gentle on the server
+                await asyncio.sleep(0.05)
             self._submitted.update(added)
 
     async def _gather_recv(self, messages: list[JsonType], timeout: float = 0.5):
@@ -255,6 +265,7 @@ class Websocket:
                 message: JsonType = json.loads(raw_message.data)
                 messages.append(message)
             elif raw_message.type is WSMsgType.CLOSE:
+                ws_logger.debug(f"Websocket[{self._idx}] received close frame: {raw_message}")
                 raise WebsocketClosed(received=True)
             elif raw_message.type is WSMsgType.CLOSED:
                 raise WebsocketClosed(received=False)
@@ -325,8 +336,23 @@ class Websocket:
         assert ws is not None
         if message["type"] != "PING":
             message["nonce"] = create_nonce(CHARS_ASCII, 30)
-        await ws.send_json(message, dumps=json_minify)
-        ws_logger.debug(f"Websocket[{self._idx}] sent: {message}")
+        # Prepare minified payload and log length before sending
+        # Prepare a safe payload string for sending; avoid leaking auth tokens
+        payload_text = json_minify(message)
+        # create a log-safe copy by masking auth_token values
+        try:
+            log_message = dict(message)
+            if "data" in log_message and isinstance(log_message["data"], dict):
+                if "auth_token" in log_message["data"]:
+                    log_message["data"]["auth_token"] = "<redacted>"
+            log_text = json_minify(log_message)
+        except Exception:
+            log_text = "<unserializable>"
+        payload_bytes = payload_text.encode("utf-8")
+        ws_logger.debug(
+            f"Websocket[{self._idx}] sending {len(payload_bytes)} bytes: {log_text}"
+        )
+        await ws.send_str(payload_text)
 
 
 class WebsocketPool:
