6ac5ffba5c1cc685063960695f86cc123ba72700
Journey <timtag1190@gmail.com>
2026-03-12 18:11:43 -0500

    Minor formatting cleanup

--- channel.py
+++ channel.py
@@ -285,9 +285,8 @@ class Channel:
 
         For mobile view, spade_url is available immediately from the page, skipping step #2.
         """
-        SETTINGS_PATTERN: str = (
-            r'src="(https://[\w.]+/config/settings\.[0-9a-f]{32}\.js)"'
-        )
+        SETTINGS_PATTERN: str = r'src="(https://[\w.]+/config/settings\.[0-9a-f]{32}\.js)"'
+        # NOTE: "beacon" appears in the file first, so it's the most likely one to be matched.
         SPADE_PATTERN: str = (
             r'"(?:spade_?url|beacon_?url)": ?"(https://[^"]+)"'
         )
